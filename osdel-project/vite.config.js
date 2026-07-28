736ff623f8c4fca9d689208904127acca81ef1bd
Asif Istiaque <153318556+Asifistiaque777@users.noreply.github.com>
2026-07-29 04:06:16 +0600

    Fix vite config to force copy public directory to dist

--- osdel-project/vite.config.js
+++ osdel-project/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true, // 👈 এটি নিশ্চিত করবে public ফোল্ডারের সাইলগুলো dist-এ কপি হবে
+  },
 })
